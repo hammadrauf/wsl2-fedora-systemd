9eaa30d1f3b648d642361274cd355c49f3d831f6
hammadrauf <rauf.hammad@gmail.com>
2024-04-03 15:30:22 -0400

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -11,3 +11,19 @@ systemd=true
 default=wsl01
 EOF
 echo "wsl01        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+cat >> /home/wsl01/.bashrc << EOF2
+
+# set DISPLAY to use X terminal in WSL
+# in WSL2 the localhost and network interfaces are not the same than windows
+## In Windows Host XServer should be running and allowed to take unauthenticated client sessions.
+## One free XServer - VcXsrv X Server - http://vcxsrv.sourceforge.net - Version 1.20.14.0
+if grep -q WSL2 /proc/version; then
+    # execute route.exe in the windows to determine its IP address
+    DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+else
+    # In WSL1 the DISPLAY can be the localhost address
+    if grep -q icrosoft /proc/version; then
+        DISPLAY=127.0.0.1:0.0
+    fi
+fi
+EOF2
